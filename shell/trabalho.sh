82917d5089f090ebdcde419cb71bceccf0617d5a
HenriqueRamos13 <henrique.mrcr@gmail.com>
2022-11-05 20:01:45 +0000

    save

--- shell/trabalho.sh
+++ shell/trabalho.sh
@@ -31,11 +31,12 @@ countLines(){
 
 buy(){
 	titulo=$(dialog --stdout --inputbox 'Titulo:' 0 0)
+	artista=$(dialog --stdout --inputbox 'Artista:' 0 0)
 	preco=$(dialog --stdout --inputbox 'Preço:' 0 0)
 	dataCompra=$(dialog --stdout --inputbox 'Data de compra:' 0 0)
 	formato=$(dialog --stdout --inputbox 'Formato:' 0 0)
 
-	echo $(($(countLines musicas.txt) + 1)):$titulo:$preco:$dataCompra:$formato >> musicas.txt
+	echo $(($(countLines musicas.txt) + 1)):${titulo:0:10}:${artista:0:10}:${preco:0:10}:${dataCompra:0:10}:${formato:0:10} >> musicas.txt
 
 	echo $preco >> compras.txt
 
@@ -60,16 +61,18 @@ seeData() {
 	for i in $(cat musicas.txt); do
 		if [[ $i != *":SOLD:"* ]]; then
 			echo $i
+			echo ""
 		fi
 	done > .temp.txt.tmp
 
-	order=$(dialog --stdout --menu 'Ordenar por:' 0 0 0 1 'Id' 2 'Titulo' 3 'Preço' 4 'Formato')
+	order=$(dialog --stdout --menu 'Ordenar por:' 0 0 0 1 'Id' 2 'Titulo' 3 'Artista' 4 'Preço' 5 'Formato')
 	
 	case $order in
-		1) dialog --textbox .temp.txt.tmp 0 0 ;;
+		1) dialog --textbox .temp.txt.tmp 200 200 ;;
 		2) orderData 1 ;;
 		3) orderData 2 ;;
 		4) orderData 3 ;;
+		5) orderData 4 ;;
 	esac
 
 	rm .temp.txt.tmp
@@ -80,15 +83,17 @@ seeData() {
 orderData(){
 	# $1 is the option that the user choose
 	# if the user choose 1 then order by the title
-	# if the user choose 2 then order by the price
-	# if the user choose 3 then order by the format
+	# if the user choose 2 then order by the artist
+	# if the user choose 3 then order by the price
+	# if the user choose 4 then order by the format
 	case $1 in
 		1) sort -t ':' -k 2 .temp.txt.tmp > .temp.txt.tmp2 ;;
-		2) sort -t ':' -k 3 .temp.txt.tmp > .temp.txt.tmp2 ;;
-		3) sort -t ':' -k 5 .temp.txt.tmp > .temp.txt.tmp2 ;;
+		2) sort -t ':' -k 3,3 .temp.txt.tmp > .temp.txt.tmp2 ;;
+		3) sort -t ':' -k 4 .temp.txt.tmp > .temp.txt.tmp2 ;;
+		4) sort -t ':' -k 5,5 .temp.txt.tmp > .temp.txt.tmp2 ;;
 	esac
 
-	dialog --textbox .temp.txt.tmp2 0 0
+	dialog --textbox .temp.txt.tmp2 200 200
 	rm .temp.txt.tmp2
 	
 	
@@ -110,15 +115,17 @@ sell(){
 alterData(){
 	id=$(dialog --stdout --inputbox 'Id:' 0 0)
 	titulo=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $2}' musicas.txt)
-	preco=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $3}' musicas.txt)
-	dataCompra=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $4}' musicas.txt)
-	formato=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $5}' musicas.txt)
+	artista=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $3}' musicas.txt)
+	preco=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $4}' musicas.txt)
+	dataCompra=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $5}' musicas.txt)
+	formato=$(awk -v id=$id 'BEGIN{FS=":"} {if(NR==id) print $6}' musicas.txt)
 
 	titulo=$(dialog --stdout --inputbox 'Titulo:' 0 0 $titulo)
+	artista=$(dialog --stdout --inputbox 'Artista:' 0 0 $artista)
 	dataCompra=$(dialog --stdout --inputbox 'Data de compra:' 0 0 $dataCompra)
 	formato=$(dialog --stdout --inputbox 'Formato:' 0 0 $formato)
 
-	awk -v id=$id -v titulo=$titulo -v preco=$preco -v dataCompra=$dataCompra -v formato=$formato 'BEGIN{FS=":"; OFS=":"} {if(NR==id) $0=id":"titulo":"preco":"dataCompra":"formato; print}' musicas.txt > musicas.txt.tmp
+	awk -v id=$id -v titulo=$titulo -v artista=$artista -v preco=$preco -v dataCompra=$dataCompra -v formato=$formato 'BEGIN{FS=":"; OFS=":"} {if(NR==id) $0=id":"titulo":"artista":"preco":"dataCompra":"formato; print}' musicas.txt > musicas.txt.tmp
 	mv musicas.txt.tmp musicas.txt
 
 	dialog --msgbox 'Alterado com sucesso!' 0 0
@@ -150,7 +157,7 @@ createBackup(){
 	cp musicas.txt ./backup/musicas.txt
 
 	dialog --msgbox 'Backup criado com sucesso!' 0 0
-	gestaoBaseDados
+	database
 }
 
 restoreBackup(){
@@ -160,7 +167,7 @@ restoreBackup(){
 	cp ./backup/musicas.txt musicas.txt
 
 	dialog --msgbox 'Backup restaurado com sucesso!' 0 0
-	gestaoBaseDados
+	database
 }
 
 deleteBackups(){
@@ -169,7 +176,7 @@ deleteBackups(){
 	rm backup/musicas.txt
 
 	dialog --msgbox 'Backup apagado com sucesso!' 0 0
-	gestaoBaseDados
+	database
 }
 
 reports(){
@@ -205,7 +212,7 @@ Média: $media" > .temp.txt.tmp
 	dialog --textbox .temp.txt.tmp 0 0
 	rm .temp.txt.tmp
 
-	relatorios
+	reports
 }
 
 relatorioVendas(){
@@ -222,7 +229,7 @@ Média: $media" > .temp.txt.tmp
 	dialog --textbox .temp.txt.tmp 0 0
 	rm .temp.txt.tmp
 
-	relatorios
+	reports
 }
 
 relatorioLucro(){
@@ -237,7 +244,7 @@ relatorioLucro(){
 	dialog --textbox .temp.txt.tmp 0 0
 	rm .temp.txt.tmp
 
-	relatorios
+	reports
 }
 
 menuPrincipal
